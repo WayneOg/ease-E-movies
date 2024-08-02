bd62eca74332d624c75340ef6625b9c491c15de0
kenyansports <kenyansportsofficial@gmail.com>
2024-08-03 00:12:45 +0300

    Update settings.py

--- moviedatabase/settings.py
+++ moviedatabase/settings.py
@@ -88,7 +88,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    'OPTIONS': {
+        'deterministic': False,
+      }
     }
+    
 }
 
 

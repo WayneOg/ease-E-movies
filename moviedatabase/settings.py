59bfd0b8e53c5d2b17641bc1035809cd1b06f530
kenyansports <kenyansportsofficial@gmail.com>
2024-08-03 00:17:21 +0300

    Update settings.py

--- moviedatabase/settings.py
+++ moviedatabase/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-import django.db.backends.sqlite3.base
-django.db.backends.sqlite3.base.DatabaseWrapper.enable_deterministic_cursors = lambda self: None
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,16 +84,10 @@ WSGI_APPLICATION = 'moviedatabase.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    'OPTIONS': {
-        'deterministic': False,
-      }
-    }
-    
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

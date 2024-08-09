fc96ab50deb540d398e37ec4e5b79fd19b7fc116
kenyansports <kenyansportsofficial@gmail.com>
2024-08-09 09:32:54 +0300

    download

--- moviedb/views.py
+++ moviedb/views.py
@@ -309,7 +309,7 @@ class SearchService:
             for result in movie_results:
                 if result.get('poster_path'):
                     movie, created = Movie.objects.update_or_create(
-                        tmdb_id=result['id'],
+                        id=result['id'],
                         defaults={
                             'title': result['title'],
                             'overview': result.get('overview', ''),
@@ -338,7 +338,7 @@ class SearchService:
             for result in series_results:
                 if result.get('poster_path'):
                     serie, created = Series.objects.update_or_create(
-                        tmdb_id=result['id'],  # assuming TMDB ID is used as the primary key
+                        id=result['id'],  # assuming TMDB ID is used as the primary key
                         defaults={
                             'title': result['name'],
                             'summary': result.get('overview', ''),
